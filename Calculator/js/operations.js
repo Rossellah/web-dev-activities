8271f23e911f1ce86cc67ade4b04a29c52b835cc
Rossellah <Rossellah.rossellahbodano526@gmail.com>
2025-01-27 09:22:49 +0800

    operations.js

--- Calculator/js/operations.js
+++ Calculator/js/operations.js
@@ -1,100 +1,97 @@
 function plus() {
-    const display1 = document.getElementById('display1');
-    const display2 = document.getElementById('display2');
-    document.getElementById('operator').value = "+";
+    const display1 = document.getElementById('display1').value;
+    const display2 = document.getElementById('display2').value;
+    const operator = document.getElementById('operator').value;
 
-    if (display2.value === "+") {
+    if (display2 === "") {
         document.getElementById('display2').value = display1;
-        document.getElementById('display1').value = "0";
-
-    } else {
-        display2.value = Number(display1.value) + Number(display2.value);
-        document.getElementById('display1').value = "0";
-
+    } else if (operator !== "") {
+        document.getElementById('display2').value = calculate(Number(display2), Number(display1), operator);
     }
+
+    document.getElementById('display1').value = "0";
+    document.getElementById('operator').value = "+";
 }
 
 function minus() {
-    const display1 = document.getElementById('display1');
-    const display2 = document.getElementById('display2');
-    document.getElementById('operator').value = "-";
+    const display1 = document.getElementById('display1').value;
+    const display2 = document.getElementById('display2').value;
+    const operator = document.getElementById('operator').value;
 
-    if (display2.value === "-") {
+    if (display2 === "") {
         document.getElementById('display2').value = display1;
-        document.getElementById('display1').value = "0";
-
-    } else {
-        display2.value = Number(display1.value) - Number(display2.value);
-        document.getElementById('display1').value = "0";
-
+    } else if (operator !== "") {
+        document.getElementById('display2').value = calculate(Number(display2), Number(display1), operator);
     }
+
+    document.getElementById('display1').value = "0";
+    document.getElementById('operator').value = "-";
 }
 
 function times() {
-    const display1 = document.getElementById('display1');
-    const display2 = document.getElementById('display2');
-    document.getElementById('operator').value = "*";
+    const display1 = document.getElementById('display1').value;
+    const display2 = document.getElementById('display2').value;
+    const operator = document.getElementById('operator').value;
 
-    if (display2.value === "") {
-        
-        document.getElementById('display2').value = display1.value;
-        document.getElementById('display1').value = "0";
-
-    } else {
-       
-        display2.value = Number(display1.value) * Number(display2.value);
-        document.getElementById('display1').value = "0";
+    if (display2 === "") {
+        document.getElementById('display2').value = display1;
+    } else if (operator !== "") {
+        document.getElementById('display2').value = calculate(Number(display2), Number(display1), operator);
     }
+
+    document.getElementById('display1').value = "0";
+    document.getElementById('operator').value = "*";
 }
 
 function div() {
-    const display1 = document.getElementById('display1');
-    const display2 = document.getElementById('display2');
-    document.getElementById('operator').value = "/";
+    const display1 = document.getElementById('display1').value;
+    const display2 = document.getElementById('display2').value;
+    const operator = document.getElementById('operator').value;
 
-    if (display2.value === "") {
-        document.getElementById('display2').value = display1.value;
-        document.getElementById('display1').value = "0";
-    } else {
-        if (Number(display1.value) === 0) {
+    if (display2 === "") {
+        document.getElementById('display2').value = display1;
+    } else if (operator !== "") {
+        if (Number(display1) === 0) {
             document.getElementById('display2').value = "Error";
         } else {
-            display2.value = Number(display2.value) / Number(display1.value);
+            document.getElementById('display2').value = calculate(Number(display2), Number(display1), operator);
         }
-        document.getElementById('display1').value = "0";
     }
-}
-   
-function dot() {
-    const display1 = document.getElementById('display1');
-    const num = display1.value;
 
-    if (!num.includes(".")) { 
-        display1.value = num + "."; 
+    document.getElementById('display1').value = "0";
+    document.getElementById('operator').value = "/";
+}
+
+function dot() {
+    const display1 = document.getElementById('display1').value;
+    if (!display1.includes(".")) {
+        document.getElementById('display1').value = display1 + ".";
     }
 }
+
 function equals() {
     const display1 = document.getElementById('display1');
     const display2 = document.getElementById('display2');
     const operator = document.getElementById('operator').value;
     let answer;
 
-    if (operator == "+") { 
+    if (operator === "+") {
         answer = Number(display2.value) + Number(display1.value);
-    } else if (operator == "-") {
+    } else if (operator === "-") {
         answer = Number(display2.value) - Number(display1.value);
-    } else if (operator == "*") {
+    } else if (operator === "*") {
         answer = Number(display2.value) * Number(display1.value);
-    } else if (operator == "/") {
+    } else if (operator === "/") {
         if (Number(display1.value) === 0) {
             answer = "Error"; 
         } else {
             answer = Number(display2.value) / Number(display1.value);
         }
+    } else {
+        answer = Number(display1.value); 
     }
 
-    display1.value = answer;
-    display2.value = "0";
-    document.getElementById('operator').value = "";
+    display1.value = answer; 
+    display2.value = ""; 
+    document.getElementById('operator').value = ""; 
 }
-
