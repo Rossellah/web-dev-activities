390a601224ed0d392b624459364732e9a14a21af
Rossellah <Rossellah.rossellahbodano526@gmail.com>
2025-01-31 09:11:17 +0800

    Loop

---
+++ Loop/js/loop.js
@@ -0,0 +1,26 @@
+for(let a=0; a,10; a++) {
+    console.log(a);
+}
+
+const numbers = [14, 4, 9, 16, 25]
+for(let c in numbers) {
+    console.log(numbers[c]);
+}
+
+const cars = ["BMW", "Volvo", "MINI"];
+for(let car of cars);{
+   console.log(cars);
+}
+
+const word = "The quick brown fox";
+for(let letter of word) {
+    console.log(letter);
+}
+
+//while loop
+let b = 20;
+while (b < 30) {
+    console.log(b);
+    b++;
+
+}
